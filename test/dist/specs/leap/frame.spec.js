dee7808d5d59241147a14a6fb03b9fb9a70ce06e
Andrew Stewart <andrew@stwrt.ca>
2013-12-06 12:15:09 -0500

    Move Leap Controller spec into leap subdir

--- test/dist/specs/leap/frame.spec.js
+++ test/dist/specs/leap/frame.spec.js
@@ -21,15 +21,6 @@
       it("stores the raw frame data", function() {
         return frame.raw.should.be.equal(frameJSON);
       });
-      it("stores the detected hands in an array", function() {
-        return frame.hands.should.be.a('array');
-      });
-      it("stores the detected gestures in an array", function() {
-        return frame.gestures.should.be.a('array');
-      });
-      it("stores the detected pointables in an array", function() {
-        return frame.pointables.should.be.a('array');
-      });
       it("extracts rotation data from the frame", function() {
         frame.rotation.should.be.a('object');
         frame.rotation.axis.should.be.a('array');
@@ -41,6 +32,15 @@
       });
       it("extracts translation factor data from the frame", function() {
         return frame.translation.should.be.a('array');
+      });
+      it("stores the detected hands in an array", function() {
+        return frame.hands.should.be.a('array');
+      });
+      it("stores the detected gestures in an array", function() {
+        return frame.gestures.should.be.a('array');
+      });
+      it("stores the detected pointables in an array", function() {
+        return frame.pointables.should.be.a('array');
       });
       return it("exposes a toString function", function() {
         return frame.toString.should.be.a('function');
