5480899018690f74c4f93f2b6a9fe9ef70a9e162
Andrew Stewart <andrew@stwrt.ca>
2014-11-04 14:25:54 -0800

    Update module registration for latest Cylon changes

--- test/specs/cylon-leapmotion.spec.js
+++ test/specs/cylon-leapmotion.spec.js
@@ -6,24 +6,15 @@ var Adaptor = source('adaptor'),
     Driver = source('driver');
 
 describe("Cylon.Leapmotion", function() {
-  describe("#register", function() {
-    var robot, adaptor, driver;
-
-    beforeEach(function() {
-      robot = { registerAdaptor: spy(), registerDriver: spy() };
-
-      adaptor = robot.registerAdaptor;
-      driver = robot.registerDriver;
-
-      module.register(robot);
+  describe("#adaptors", function() {
+    it('is an array of supplied adaptors', function() {
+      expect(module.adaptors).to.be.eql(['leapmotion']);
     });
+  });
 
-    it("registers the LeapMotion adaptor", function() {
-      expect(adaptor).to.be.calledWith('cylon-leapmotion', 'leapmotion');
-    });
-
-    it("registers the LeapMotion driver", function() {
-      expect(driver).to.be.calledWith('cylon-leapmotion', 'leapmotion');
+  describe("#drivers", function() {
+    it('is an array of supplied drivers', function() {
+      expect(module.drivers).to.be.eql(['leapmotion']);
     });
   });
 
